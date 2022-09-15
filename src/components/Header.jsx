62c7b28ce1abd520ca9c0fd056c0456a6db14812
Lofibree <nastenkapersik17@gmail.com>
2022-09-15 14:53:38 +0300

    init commit 2

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <header className='header'>
+                <img src='https://cdn.logo.com/hotlink-ok/logo-social.png' />
+            </header>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div>
+            <div className='content'>
+        <div>
+          <img src='https://sun9-8.userapi.com/impg/SPXuy_IeIvIx5G9GnWliHWaHt4XsdBtSy6QPGA/kStdPlFXzGY.jpg?size=811x722&quality=95&sign=b2307ccbf1448ea99f545b2e9309b699&type=album' />
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <div>
+            Post1
+          </div>
+          <div>
+            Post2
+          </div>
+        </div>
+      </div>
+        </div>
+    );
+};
+
+export default Profile;
